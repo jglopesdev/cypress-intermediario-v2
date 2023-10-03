2c727483e648ddfc7d4740f7408cfc09513ec8ef
João Lopes <jgsantanalopes@gmail.com>
2023-10-03 08:54:26 -0300

    initial commit

---
+++ cypress/e2e/gui/logout.cy.js
@@ -0,0 +1,13 @@
+describe('Logout', () => {
+    beforeEach(() => {
+      cy.login()
+      cy.visit('/')
+    })
+  
+    it('successfully', () => {
+      cy.logout()
+  
+      cy.url().should('be.equal', `${Cypress.config('baseUrl')}/users/sign_in`)
+    })
+  })
+  
---
+++ cypress/support/e2e.js
@@ -0,0 +1,6 @@
+import 'cypress-plugin-api'
+
+import './api_commands'
+import './cli_commands'
+import './gui_commands'
+
